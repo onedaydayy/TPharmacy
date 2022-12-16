b9484588e1b944182d281736004303f96829d3c1
onedaydayy <2103769b@student.tp.edu.sg>
2022-12-16 14:07:39 +0800

    update seeding - customer & product

---
+++ TPharmacy/Server/Configuration/Entities/CustomerSeedConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using TPharmacy.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TPharmacy.Server.Configuration.Entities
+{
+    public class CustomerSeedConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasData(
+                    new Customer
+                    {
+                        ID = 1,
+                        CusName = "James",
+                        CusEmail = "james@gmail.com",
+                        CusAddress = "Tampines Ave 1",
+                        CusPassword = "James1234*",
+                        CusPoints = 100,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    },
+                    new Customer
+                    {
+                        ID = 2,
+                        CusName = "Alice",
+                        CusEmail = "alice@gmail.com",
+                        CusAddress = "Bedok Ave 1",
+                        CusPassword = "Alice1234*",
+                        CusPoints = 50,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    },
+                    new Customer
+                    {
+                        ID = 3,
+                        CusName = "Grey",
+                        CusEmail = "grey@gmail.com",
+                        CusAddress = "Temasek Polytechnic",
+                        CusPassword = "Grey1234*",
+                        CusPoints = 89,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    }
+                    );
+        }
+
+    }
+}
---
+++ TPharmacy/Server/Configuration/Entities/ProductSeedConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using TPharmacy.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TPharmacy.Server.Configuration.Entities
+{
+    public class ProductSeedConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                    new Product
+                    {
+                        ID = 1,
+                        ProdBrand = "Blackmores",
+                        ProdName = "Vitamin C",
+                        ProdDesc = "BLACKMORES Bio 1000mg Vitamin C 120 Tablets",
+                        ProdPrice = 35,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    },
+                    new Product
+                    {
+                        ID = 2,
+                        ProdBrand = "Swisse",
+                        ProdName = "Omega 3 Fish Oil",
+                        ProdDesc = "Omega 3 Fish Oil 1500mg 100 Tablets",
+                        ProdPrice = 58,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    },
+                    new Product
+                    {
+                        ID = 3,
+                        ProdBrand = "Wastons",
+                        ProdName = "Calcium",
+                        ProdDesc = "Calcium 500mg 150 Tablets",
+                        ProdPrice = 45,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    }
+                    );
+        }
+
+    }
+}
