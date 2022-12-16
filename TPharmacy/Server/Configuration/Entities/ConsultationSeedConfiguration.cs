bc9b4775df4995dfa73381acb594948f0009854d
onedaydayy <2103769b@student.tp.edu.sg>
2022-12-16 15:28:55 +0800

    complete seeding prescription & consultation

---
+++ TPharmacy/Server/Configuration/Entities/ConsultationSeedConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using TPharmacy.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TPharmacy.Server.Configuration.Entities
+{
+    public class ConsultationSeedConfiguration : IEntityTypeConfiguration<Consultation>
+    {
+        public void Configure(EntityTypeBuilder<Consultation> builder)
+        {
+            builder.HasData(
+                    new Consultation
+                    {
+                        ID = 1,
+                        ConDate = DateTime.Now,
+                        ConStartTime = DateTime.Now,
+                        ConEndTime = DateTime.Now.AddHours(+1.15),
+                        ConFee = 35.50m,
+                        ConDesc = "Flu Consultation",
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "Woon",
+                        UpdatedBy = "System"
+                    },
+                    new Consultation
+                    {
+                        ID = 2,
+                        ConDate = DateTime.Now,
+                        ConStartTime = DateTime.Now,
+                        ConEndTime = DateTime.Now.AddHours(+1),
+                        ConFee = 30.50m,
+                        ConDesc = "Eczema Consultation",
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "Woon",
+                        UpdatedBy = "System"
+                    }
+                    );
+        }
+
+    }
+}
---
+++ TPharmacy/Server/Configuration/Entities/PrescriptionSeedConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using TPharmacy.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TPharmacy.Server.Configuration.Entities
+{
+    public class PrescriptionSeedConfiguration : IEntityTypeConfiguration<Prescription>
+    {
+        public void Configure(EntityTypeBuilder<Prescription> builder)
+        {
+            builder.HasData(
+                    new Prescription
+                    {
+                        ID = 1,
+                        PresQty = 3,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    },
+                    new Prescription
+                    {
+                        ID = 2,
+                        PresQty = 2,
+                        DateCreated = DateTime.Now,
+                        DataUpdated = DateTime.Now,
+                        CreatedBy = "System",
+                        UpdatedBy = "System"
+                    }
+                    );
+        }
+
+    }
+}
